36800f42972384bf4e0e45e9bd303e1353cecb42
asa110 <133434483+asa110@users.noreply.github.com>
2023-05-22 12:08:11 +0300

    Add files via upload

---
+++ recevier.m
@@ -0,0 +1,18 @@
+clear
+clc
+tee = transmitter();
+rx_signal=[];
+ function recevied = rec(line_coding_style)
+        if (strcmp(line_coding_style,'unrz')==1)
+          for i = 1 : tee.stream_size * 2
+              if ((tee.line_coded_stream(i)) > 0.6)
+               rx_signal(i)=1;
+              endif
+          endfor
+        endif
+endfunction
+
+
+
+
+
